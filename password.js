be5d4fa403467ef1b1cf9d60ea797f025aeb2ad1
sgarg2901 <41927337+sgarg2901@users.noreply.github.com>
2022-06-19 20:53:12 +0530

    Add files via upload

---
+++ password.js
@@ -0,0 +1,94 @@
+let state = false;
+let password = document.getElementById("password");
+let passwordStrength = document.getElementById("password-strength");
+let lowUpperCase = document.querySelector(".low-upper-case i");
+let number = document.querySelector(".one-number i");
+let specialChar = document.querySelector(".one-special-char i");
+let eightChar = document.querySelector(".eight-character i");
+
+password.addEventListener("keyup", function(){
+    let pass = document.getElementById("password").value;
+    checkStrength(pass);
+});
+
+function toggle(){
+    if(state){
+        document.getElementById("password").setAttribute("type","password");
+        state = false;
+    }else{
+        document.getElementById("password").setAttribute("type","text")
+        state = true;
+    }
+}
+
+function myFunction(show){
+    show.classList.toggle("fa-eye-slash");
+}
+
+function checkStrength(password) {
+    let strength = 0;
+
+    //If password contains both lower and uppercase characters
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+        strength += 1;
+        lowUpperCase.classList.remove('fa-circle');
+        lowUpperCase.classList.add('fa-check');
+    } else {
+        lowUpperCase.classList.add('fa-circle');
+        lowUpperCase.classList.remove('fa-check');
+    }
+    //If it has numbers and characters
+    if (password.match(/([0-9])/)) {
+        strength += 1;
+        number.classList.remove('fa-circle');
+        number.classList.add('fa-check');
+    } else {
+        number.classList.add('fa-circle');
+        number.classList.remove('fa-check');
+    }
+    //If it has one special character
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+        strength += 1;
+        specialChar.classList.remove('fa-circle');
+        specialChar.classList.add('fa-check');
+    } else {
+        specialChar.classList.add('fa-circle');
+        specialChar.classList.remove('fa-check');
+    }
+    //If password is greater than 7
+    if (password.length > 7) {
+        strength += 1;
+        eightChar.classList.remove('fa-circle');
+        eightChar.classList.add('fa-check');
+    } else {
+        eightChar.classList.add('fa-circle');
+        eightChar.classList.remove('fa-check');   
+    }
+}
+function specialcharecter()
+
+            {
+
+                var iChars = "!`@#$%^&*()+=-[]\\\';,./{}|\":<>?~_";   
+
+                var data = document.getElementById("txtCallSign").value;
+
+                for (var i = 0; i < data.length; i++)
+
+                {      
+
+                    if (iChars.indexOf(data.charAt(i)) != -1)
+
+                    {    
+
+                    alert ("Your string has special characters. \nThese are not allowed.");
+
+                    document.getElementById("txtCallSign").value = "";
+
+                    return false; 
+
+                    } 
+
+                }
+
+            }
